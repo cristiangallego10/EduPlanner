737daa397673406a2c98099c8ceb3c266f9f5d2c
daniloj2023 <danilounicordoba@gmail.com>
2024-11-30 11:45:06 -0500

    Update Calendar.vue

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -1,47 +1,4 @@
-<template>
-  <div>
-    <v-sheet class="d-flex" height="54" tile>
-      <v-select
-        v-model="type"
-        :items="types"
-        class="ma-2"
-        label="View Mode"
-        variant="outlined"
-        dense
-        hide-details
-      ></v-select>
-      <v-select
-        v-model="weekday"
-        :items="weekdays"
-        class="ma-2"
-        label="Weekdays"
-        variant="outlined"
-        dense
-        hide-details
-      ></v-select>
-    </v-sheet>
-    <v-sheet>
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :events="eventsFromStore"
-        :view-mode="type"
-        :weekdays="weekday"
-      ></v-calendar>
-    </v-sheet>
-  </div>
-</template>
-<script setup>
-    const type= 'month';
-    const types = ['month', 'week', 'day'];
-    const weekday = [0, 1, 2, 3, 4, 5, 6];
-    const weekdays = [
-      { title: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-      { title: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-      { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { title: 'Mon, Wed, Fri', value: [1, 3, 5] },
-    ];
-    const value = [new Date()];
+
   
   /*computed: {
     // Usa una propiedad computada para acceder a los eventos desde el store
