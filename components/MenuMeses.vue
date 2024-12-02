b47814cf41ab195b8c72dbc3f69aa0ada36f9148
daniloj2023 <danilounicordoba@gmail.com>
2024-12-01 20:00:23 -0500

    Update MenuMeses.vue

--- components/MenuMeses.vue
+++ components/MenuMeses.vue
@@ -1,49 +1,59 @@
 <template>
-    <div class="text-center">
-      <v-select
-        v-model="location"
-        :items="locations"
-        label="Location"
-      ></v-select>
-      <v-menu :location="location">
-        <template v-slot:activator="{ props }">
-          <v-btn
-            color="primary"
-            dark
-            v-bind="props"
-          >
-            Dropdown
-          </v-btn>
-        </template>
-  
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in items"
-            :key="index"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        items: [
-          { title: 'Click Me' },
-          { title: 'Click Me' },
-          { title: 'Click Me' },
-          { title: 'Click Me 2' },
-        ],
-        locations: [
-          'top',
-          'bottom',
-          'start',
-          'end',
-          'center',
-        ],
-        location: 'end',
-      }),
-    }
-  </script>
+  <div class="text-center">
+    <!-- Selección de Meses -->
+    <v-select
+      v-model="selectedMonth"  <!-- Aquí se almacena el mes seleccionado -->
+      :items="months"           <!-- Los meses disponibles -->
+      label="Seleccionar Mes"
+    ></v-select>
+
+    <!-- Menú de ubicación (se mantiene si lo necesitas) -->
+    <v-menu :location="location">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="props"
+        >
+          Dropdown
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    // Meses disponibles
+    months: [
+      'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ],
+    selectedMonth: null,  // Aquí se almacena el mes seleccionado
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ],
+    locations: [
+      'top',
+      'bottom',
+      'start',
+      'end',
+      'center',
+    ],
+    location: 'end',
+  }),
+}
+</script>
